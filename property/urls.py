aae750e063ca609104da37f504ec1ff5851e141e
Kaung Myat Han <arkarhtethan@gmail.com>
2019-07-02 00:58:19 +0630

    Property listing is ready

--- property/urls.py
+++ property/urls.py
@@ -6,4 +6,5 @@ app_name = 'property'
 urlpatterns = [
     path('', views.PropertyHomeView.as_view(), name="home"),
     path('property-list/', views.PropertyListView.as_view(), name="list"),
+    path('property-detail/<int:pk>/', views.PropertyDetailView.as_view(), name="detail"),
 ]
